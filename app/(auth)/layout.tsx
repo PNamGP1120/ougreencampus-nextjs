2591b4c7f9996a2dc50d7daebb65561e16309d74
PNamGP1120 <phuongnam.it0212@gmail.com>
2025-12-29 03:36:01 +0700

    fix: make auth layout client to enable form submit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 export default function AuthLayout({
                                        children,
                                    }: {
     children: React.ReactNode;
 }) {
     return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            <div className="w-full max-w-md">{children}</div>
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+            {children}
         </div>
     );
 }
